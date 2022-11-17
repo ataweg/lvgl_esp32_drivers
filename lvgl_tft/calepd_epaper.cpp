49cc58f25fcec2a6bf3c247787f73c69ed90fc9e
Goran Mahovlic <goran.mahovlic@gmail.com>
2022-11-17 17:52:16 +0100

    Small fixes for e-epaper B&W

--- lvgl_tft/calepd_epaper.cpp
+++ lvgl_tft/calepd_epaper.cpp
@@ -16,10 +16,9 @@
 // Select the right class for your SPI epaper: https://github.com/martinberlin/cale-idf/wiki
 //#include <gdew0583t7.h>
 #include <gdew042t2.h>
+EpdSpi io;
+Gdew042t2 display(io);
 
-EpdSpi IO;
-Gdew042t2 display(IO);
-//Gdew0583T7 display(io);
 
 /** test Display dimensions
  * Do not forget to set: menuconfig -> Components -> LVGL configuration
@@ -40,29 +39,8 @@ void calepd_init(void)
     printf("calepd_init\n");
     display.init();
     display.setRotation(0);
-
-
-      display.setTextColor(EPD_BLACK, EPD_WHITE); // Set text color to be black and background color to be white
-      display.setTextSize(2);             // Set text to be 4 times bigger than classic 5x7 px text
-
-      // Prepare message text and position
-      char message[] = "Init done!";
-      int w = display.width();
-      int h = display.height();
-      int cursor_x = (w / 2) - (strlen(message) / 2);
-      int cursor_y = h / 2;
-
-      ESP_LOGI(TAG, "Display size: width: %d, height: %d", w, h);
-
-      // Write text to the center of the screen
-      display.setCursor(cursor_x, cursor_y);
-      display.print(message);
-
-      // Do a partial update and print the text to the screen
-      display.update();
-
     // Clear screen
-    //display.update();
+    display.update();
 }
 
 /* Required by LVGL */
@@ -70,20 +48,22 @@ void calepd_flush(lv_disp_drv_t *drv, const lv_area_t *area, lv_color_t *color_m
 {
     ++flushcalls;
     printf("flush %d x:%d y:%d w:%d h:%d\n", flushcalls,area->x1,area->y1,lv_area_get_width(area),lv_area_get_height(area));
-
+    printf("w:%d h:%d\n", display.width(),display.height());
     // Full update
-    if (lv_area_get_width(area)==display.width() && lv_area_get_height(area)==display.height()) {
-      display.update();
-    } else {
+    //if (area->x1 == 400 && area->y1 == 300) {
+    display.update();
+      //lv_disp_flush_ready(drv);
+    //} 
+    //else {
       // Partial update:
-      display.update(); // Uncomment to disable partial update
+    //  display.update(); // Uncomment to disable partial update
       //display.updateWindow(area->x1,area->y1,lv_area_get_width(area),lv_area_get_height(area), true);
       //display.updateWindow(area->x1,area->y1,lv_area_get_width(area),lv_area_get_height(area));
-    }
-
+    //}
+    lv_disp_flush_ready(drv);
     /* IMPORTANT!!!
      * Inform the graphics library that you are ready with the flushing */
-    lv_disp_flush_ready(drv);
+    //lv_disp_flush_ready(drv);
 }
 
 /* Called for each pixel */
@@ -93,14 +73,19 @@ void calepd_set_px_cb(lv_disp_drv_t * disp_drv, uint8_t* buf,
 {
     //If not drawing anything: Debug to see if this function is called:
     //printf("set_px %d %d\n",(int16_t)x,(int16_t)y);
-
+    
     // Test using RGB232
-    int16_t epd_color = EPD_WHITE;
-
+    //int16_t epd_color = EPD_WHITE;
+    //printf("buf_w:%d x:%d y:%d color:%d\n", buf_w, x, y,(int16_t)color.full);
     // Color setting use: RGB232
     // Only monochrome:All what is not white, turn black
-    if ((int16_t)color.full<254) {
-        epd_color = EPD_BLACK;
+   // if ((int16_t)color.full<254) {
+   //     epd_color = EPD_BLACK;        
+   // }
+    if ((int16_t)color.full >= 1) {
+      display.drawPixel((int16_t)x, (int16_t)y, EPD_WHITE);
     }
-    display.drawPixel((int16_t)x, (int16_t)y, epd_color);
+    else{
+      display.drawPixel((int16_t)x, (int16_t)y, EPD_BLACK);
+    }
 }
